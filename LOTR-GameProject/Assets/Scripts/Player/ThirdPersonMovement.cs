e36e1d6b77af30a69bd30b075757799ced34b7ed
Guilherme Henrique dos Santos Simionato <devsanom@gmail.com>
2021-04-15 17:12:39 -0300

    Moviment atualizado

---
+++ LOTR-GameProject/Assets/Scripts/Player/ThirdPersonMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+    }
+}
