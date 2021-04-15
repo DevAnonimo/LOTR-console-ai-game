5a58d0d32bd31e101f32338959bc4c5a4916e183
Kaio Rauta <kaio_soares_rauta@yahoo.com.br>
2021-04-14 23:02:49 -0300

    Adicionado implementação inicial de Steering Behaviour dos chefes

---
+++ LOTR-GameProject/Assets/Scripts/BaseBossAgent.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class BaseBossAgent : MonoBehaviour
+    {
+        public float maxSpeed = 10.0f;
+        public float maxAcceleration;
+
+        public float orientation;
+        public float rotation;
+        public Vector3 Velocity;
+        public Steering Steer;
+
+        public float maxRotation = 45.0f;
+        public float maxAngularAcceleration = 45.0f;
+
+        private void Start()
+        {
+            Velocity = Vector3.zero;
+            Steer = new Steering();
+        }
+
+        private void Update()
+        {
+            var displacement = Velocity * Time.deltaTime;
+            displacement.y = 0;
+            orientation += rotation * Time.deltaTime;
+            orientation = CapOrientation(orientation);
+
+            Transform thisTransform;
+
+            (thisTransform = transform).Translate(displacement, Space.World);
+            thisTransform.rotation = new Quaternion();
+            transform.Rotate(Vector3.up, orientation);
+        }
+
+        private void LateUpdate()
+        {
+            Velocity += Steer.Linear * Time.deltaTime;
+            rotation += Steer.Angular * Time.deltaTime;
+
+            Velocity = CapVelocity(Velocity);
+            if (Steer.Linear.magnitude == 0.0f)
+                Velocity = Vector3.zero;
+
+            Steer = new Steering();
+        }
+
+        public void SetSteering(Steering steer, float weight)
+        {
+            Steer.Linear += weight * steer.Linear;
+            Steer.Angular += weight * steer.Angular;
+        }
+
+        /// <summary>
+        /// Limita a velocidade deste agente ao máximo configurado
+        /// </summary>
+        /// <param name="currentVelocity">o Vector3 que indica a velocidade atual</param>
+        /// <returns>A velocidade corrigida</returns>
+        private Vector3 CapVelocity(Vector3 currentVelocity)
+        {
+            if (!(currentVelocity.magnitude > maxSpeed))
+                return currentVelocity;
+
+            currentVelocity.Normalize();
+            currentVelocity *= maxSpeed;
+
+            return currentVelocity;
+        }
+
+        /// <summary>
+        /// Limita a orientação entre 0 e 360 graus
+        /// </summary>
+        /// <param name="oldOrientation"></param>
+        /// <returns>A orientação limitada</returns>
+        private float CapOrientation(float oldOrientation)
+        {
+            if (oldOrientation < 0.0f)
+                oldOrientation += 360.0f;
+            else if (oldOrientation > 360.0f)
+                oldOrientation -= 360.0f;
+
+            return oldOrientation;
+        }
+    }
+}
---
+++ LOTR-GameProject/Assets/Scripts/BossBaseBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class BossBaseBehaviour : MonoBehaviour
+    {
+        public float weight = 1.0f;
+
+        public GameObject target;
+        protected BaseBossAgent agent;
+        public Vector3 destination;
+
+        public float maxSpeed = 30.0f;
+        public float maxAcceleration = 30.0f;
+        public float maxRotation = 10.0f;
+        public float maxAngularAcceleration = 10.0f;
+
+        public virtual void Start()
+        {
+            agent.gameObject.GetComponent<BaseBossAgent>();
+        }
+
+        public virtual void Update()
+        {
+            agent.SetSteering(GetSteering(), weight);
+        }
+
+        private float MapToRange(float rotation)
+        {
+            rotation %= 360.0f;
+            if (!(Mathf.Abs(rotation) > 180.0f))
+                return rotation;
+
+            if (rotation < 0.0f)
+                rotation += 360.0f;
+            else
+                rotation -= 360.0f;
+
+            return rotation;
+        }
+
+        public virtual Steering GetSteering()
+            => new Steering();
+    }
+}
