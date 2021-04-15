e3158ef35524a34bf185724daad3ed3f8208649f
Guilherme Simionato <devsanom@gmail.com>
2021-04-15 03:05:20 -0300

    Movimentação personagem
    Pulo adicionado mas movimentação ta bugada

--- LOTR-GameProject/Assets/Scripts/Player/PlayerController.cs
+++ LOTR-GameProject/Assets/Scripts/Player/PlayerController.cs
@@ -5,27 +5,41 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float moveSpeed = 20.0f;
-    private Vector3 direction;
-    private CharacterController characterController;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        controller = gameObject.GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        direction.x = Input.GetAxis("Horizontal");
-        direction.z = Input.GetAxis("Vertical");
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
 
-        //if (Input.GetKeyDown(KeyCode.Space))
-            direction.y = Input.GetAxis("Jump");
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
 
-        Vector3 moveDirection = new Vector3(direction.x, 0, direction.z);
-        characterController.SimpleMove(moveDirection * moveSpeed);
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
