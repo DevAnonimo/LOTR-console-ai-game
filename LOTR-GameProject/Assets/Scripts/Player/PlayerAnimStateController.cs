1c6a38463a355da4e5faa79daf8f4e04ac404b7d
Guilherme Simionato <devsanom@gmail.com>
2021-04-16 04:28:19 -0300

    Movimento do player melhorado
    Está em 50%.
    Falta:
     - Bug com a letra "s";
     - Ataque inicial.

---
+++ LOTR-GameProject/Assets/Scripts/Player/PlayerAnimStateController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimStateController : MonoBehaviour
+{
+
+    Animator animator;
+    int isRunningHash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isRunningHash = Animator.StringToHash("isRunning");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isRunning = animator.GetBool(isRunningHash);
+        bool forwardPressed = Input.GetKey("w");
+
+        //w key is pressed
+        if (!isRunning && forwardPressed)
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+
+        //w key is not pressed
+        if (isRunning && !forwardPressed)
+        {
+            animator.SetBool(isRunningHash, false);
+        }
+    }
+}
