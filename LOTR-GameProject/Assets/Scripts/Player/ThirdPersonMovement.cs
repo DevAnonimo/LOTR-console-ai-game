c39f869ade917922eb86bc571bc447c4908f3359
Kaio Rauta <kaio_soares_rauta@yahoo.com.br>
2021-04-17 02:05:33 -0300

    Salvas alterações na Cena de desenvolvimento do Steering Behaviour

--- LOTR-GameProject/Assets/Scripts/Player/ThirdPersonMovement.cs
+++ LOTR-GameProject/Assets/Scripts/Player/ThirdPersonMovement.cs
@@ -44,7 +44,7 @@ public class ThirdPersonMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            controller.Move(moveDir.normalized * (speed * Time.deltaTime));
         }
 
 
