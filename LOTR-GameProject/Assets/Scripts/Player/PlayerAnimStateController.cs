dff08a087087d27acda64bf78dea4ad25ceeaf4a
Guilherme Simionato <devsanom@gmail.com>
2021-04-20 02:14:54 -0300

    Animações - Scripts e arquivo Blend
    -Animações de Sword Idle e Sword Run;
    -Script de controle das animações melhorado
    -Arquivo blend como backup

--- LOTR-GameProject/Assets/Scripts/Player/PlayerAnimStateController.cs
+++ LOTR-GameProject/Assets/Scripts/Player/PlayerAnimStateController.cs
@@ -4,21 +4,26 @@ namespace Player
 {
     public class PlayerAnimStateController : MonoBehaviour
     {
+        public GameObject sword;
 
         Animator animator;
         int isRunningHash;
+        int hasSwordHash;
 
         // Start is called before the first frame update
         void Start()
         {
             animator = GetComponent<Animator>();
             isRunningHash = Animator.StringToHash("isRunning");
+            hasSwordHash = Animator.StringToHash("hasSword");
+
         }
 
         // Update is called once per frame
         void Update()
         {
             bool isRunning = animator.GetBool(isRunningHash);
+            bool hasSword = animator.GetBool(hasSwordHash);
             bool forwardPressed = Input.GetKey("w");
 
             //w key is pressed
@@ -32,6 +37,10 @@ namespace Player
             {
                 animator.SetBool(isRunningHash, false);
             }
+
+            //has sword?
+            if (sword.activeSelf)
+                animator.SetBool(hasSwordHash, true);
         }
 
         public void AttackAnim()
