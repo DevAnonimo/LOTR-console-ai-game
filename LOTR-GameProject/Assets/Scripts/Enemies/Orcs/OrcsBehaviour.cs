290a9aed7581bcdfd749a4dd5812a02c73cf655e
Kaio Rauta <kaio_soares_rauta@yahoo.com.br>
2021-05-29 00:10:35 -0300

    Adicionado arquivos dos inimigos Orcs

---
+++ LOTR-GameProject/Assets/Scripts/Enemies/Orcs/OrcsBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using SimpleEnemy;
+using UnityEngine;
+using static LOTR_LowPoly.Sauron.SauronAnimatorVariableNamesConstants;
+
+namespace LOTR_LowPoly.Orcs
+{
+    public sealed class OrcsBehaviour : BaseBehaviour
+    {
+    }
+}
