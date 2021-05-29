77e83ee8cf715c0df99b1136be5cbd387cd214f1
Guilherme Simionato <devsanom@gmail.com>
2021-05-29 08:03:07 -0300

    "terminado"

--- LOTR-GameProject/Assets/Scripts/Menu/levelLoader.cs
+++ LOTR-GameProject/Assets/Scripts/Menu/levelLoader.cs
@@ -12,7 +12,18 @@ namespace Scripts.Menu
 
         public void LoadNextLevel()
         {
+            if (SceneManager.GetActiveScene().buildIndex == 3 || SceneManager.GetActiveScene().buildIndex == 4)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                SceneManager.LoadScene(0);
+            }
             StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        }
+
+        public void Dead()
+        {
+            StartCoroutine(LoadLevel(0));
         }
 
         private IEnumerator LoadLevel(int levelIndex)
